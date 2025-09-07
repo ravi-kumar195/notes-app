cbfa0b21497b3841962aba7f5d19bc24d2592a2f
ravi-kumar195 <ravvikumar272@gmail.com>
2025-09-07 21:04:59 +0530

    Update home.js

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -40,7 +40,7 @@ export const Home = () => {
       <main className="flex gap-3">
         <Sidebar />
         <div className="flex flex-col  w-screen mt-7">
-          <div className="flex flex-col w-60 relative self-center">
+          <div className="flex flex-col w-[450px] relative self-center">
             <input
               value={title}
               onChange={onTitleChange}
